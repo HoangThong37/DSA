a5ec5e500e86a6e7068d1ba8d14f139f240929c6
HOANG TRUNG THONG(HOANG TRUNG THONG <hoangthong@lgcns.com>
2025-04-02 16:58:46 +0700

    code leetcode

--- src/hashtable/HashTable.java
+++ src/hashtable/HashTable.java
@@ -1,2 +1,33 @@
-package hash_tables;public class HashTable {
+package hashtable;
+
+public class HashTable {
+    private int size = 7;
+    private Node[] dataMap;
+
+    class Node {
+        String key;
+        int value;
+        Node next;
+
+        public Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public void printTable() {
+        for (int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("   {" + temp.key + "= " + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+    }
+
 }
--- src/trees/BinarySearchTree.java
+++ src/trees/BinarySearchTree.java
@@ -1,2 +1,60 @@
-package trees;public class BinarySearchTree {
+package trees;
+
+public class BinarySearchTree {
+
+    public Node root;
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node current = root;
+        while (true) {
+            if (newNode.value < current.value) {
+                if (current.left == null) {
+                    current.left = newNode;
+                    return true;
+                }
+                current = current.left;
+            } else {
+                if (current.right == null) {
+                    current.right = newNode;
+                    return true;
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if(root == null) return false;
+
+        Node temp = root;
+        while(temp != null) {
+
+            if(value < temp.value) {
+                temp = temp.left;
+            } else if(value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
